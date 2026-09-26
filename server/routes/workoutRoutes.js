6ae0b8a1edd0834ff83ecdb673d8980ed7ff7201
Shahzaib1106 <shahzaibahmad1106@gmail.com>
2026-09-26 12:27:35 +0500

    feat: add exercise library and workout APIs

--- server/routes/workoutRoutes.js
+++ server/routes/workoutRoutes.js
@@ -1,10 +1,35 @@
+
 import express from "express";
 
-import { getWorkouts } from "../controllers/workoutController.js";
+import {
+  getWorkouts,
+  getWorkoutById,
+  getExercises,
+  getExerciseById,
+} from "../controllers/workoutController.js";
+
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", authMiddleware, getWorkouts);
+
+router.get(
+  "/exercises",
+  authMiddleware,
+  getExercises
+);
+
+router.get(
+  "/exercises/:id",
+  authMiddleware,
+  getExerciseById
+);
+
+router.get(
+  "/:id",
+  authMiddleware,
+  getWorkoutById
+);
 
 export default router;
